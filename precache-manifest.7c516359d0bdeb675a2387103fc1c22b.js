9c45d32a0ccbb260cb00c50b9f67edab7b149183
atasoyfurkan <=>
2020-08-31 04:33:57 +0300

    Updates

---
+++ precache-manifest.7c516359d0bdeb675a2387103fc1c22b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a865ecf38e17ef329cbdd50675d934b9",
+    "url": "/song-recommender/index.html"
+  },
+  {
+    "revision": "6df1bb8c31a571920948",
+    "url": "/song-recommender/static/css/main.31a2bec8.chunk.css"
+  },
+  {
+    "revision": "7047dcf67b182dc862f9",
+    "url": "/song-recommender/static/js/2.601030d2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/song-recommender/static/js/2.601030d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6df1bb8c31a571920948",
+    "url": "/song-recommender/static/js/main.6934e27a.chunk.js"
+  },
+  {
+    "revision": "d757c4797503c2d89833",
+    "url": "/song-recommender/static/js/runtime-main.ea639347.js"
+  }
+]);
